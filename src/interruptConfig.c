002d2e25f295a53362a1c369afa31eb63bf6f726
Ovidiu22 <75970114+Ovidiu22@users.noreply.github.com>
2022-12-28 16:00:34 +0100

    Full implementation and testing

--- src/interruptConfig.c
+++ src/interruptConfig.c
@@ -1,14 +1,18 @@
-/*
- * interruptConfig.c
- *
- * Created: 28.12.2022 14:40:04
- *  Author: lenovo
- */ 
+/*******************************/
+/*** Created by Ovidiu Sabau ***/
+/***  28th December 2022	 ***/
+/*******************************/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include "interruptConfig.h"
 
-
+ /* *****************************************************************
+ Name:		enable_interrupts()
+ Inputs:	none
+ Outputs:	none
+ Description:enables interrupt INT0 (pin D2 and sets falling condition for firing) 
+ ******************************************************************** */
  void enable_interrupts()
  {
 	 EIMSK |= (1<<INT0);
@@ -16,8 +20,13 @@
 	 sei();
  }
  
+ /* *****************************************************************
+ Name:		interruptConfig()
+ Inputs:	none
+ Outputs:	none
+ Description:sets PIND2 as input 
+ ******************************************************************** */
  void interruptConfig()
  {
 	 DDRD &= ~(1<<PIND2);	// Input
-	 //PORTD &= ~(1<<PIND2);	// Low
  }
--- src/main.c
+++ src/main.c
@@ -7,11 +7,12 @@
 #include "interruptConfig.h"
 
 void blink_LED_PORTD(uint8_t, uint8_t);
-
 volatile int toggle = 0;
+
 
 int main(void)
 {
+	/* Initialization */
 	interruptConfig();
 	enable_interrupts();
 	
@@ -19,19 +20,32 @@ int main(void)
 	{
 		if (toggle == 1)
 		{
-			blink_LED_PORTD(PIND7, 2);
+			blink_LED_PORTD(PIND7, 3);
 			toggle = 0;
 		}
 	}
+	
+	return 0;
 }
 
-
+/* *****************************************************************
+Name:		Interrupt service routine 0
+Inputs:		none
+Outputs:	none
+Description:sets toggle = 1 when an external interrupt on pin PIND2 occurs
+******************************************************************** */
 ISR(INT0_vect)
 {
 	toggle = 1;
+	_delay_ms(50);	// prevent pin toggling due to imperfect switching
 }
 
-
+/* *****************************************************************
+Name:		blink_LED_PORTD()
+Inputs:		pin number of the LED and how many times LED should blink
+Outputs:	none
+Description:blinks a LED a defined number of times
+******************************************************************** */
 void blink_LED_PORTD(uint8_t pinNumber, uint8_t times)
 {
 	DDRD |= (1 << pinNumber);
